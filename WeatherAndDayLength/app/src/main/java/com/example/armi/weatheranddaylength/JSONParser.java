5d1a1c8d0bb17f686b590c21dad5af886718e42b
Armi Korhonen <c5koar00@students.oamk.fi>
2016-11-29 21:58:30 +0200

    Mobiilitietoliikenne-projekti. Nimestään huolimatta tämä Android-applikaatio hakee osoitteen perusteella auringon nousun ja laskun tiedot ja esittää ne käyttäjälle.

---
+++ WeatherAndDayLength/app/src/main/java/com/example/armi/weatheranddaylength/JSONParser.java
@@ -0,0 +1,65 @@
+package com.example.armi.weatheranddaylength;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+
+//Yleiskäyttöinen JSON parser, millä käsitellään kaikki kolme JSON datahakua.
+public class JSONParser {
+    public static List<String> parseFeed(String content, int parseIndex){
+
+        try {
+
+            JSONObject object = new JSONObject(content);
+
+            if (parseIndex == 1) {
+
+                JSONArray ar = object.getJSONArray("results");
+                JSONObject obj = ar.getJSONObject(0);
+                JSONObject geo = obj.getJSONObject("geometry");
+                JSONObject loc = geo.getJSONObject("location");
+                String lat = loc.getString("lat");
+                String lng = loc.getString("lng");
+                List<String> jsonList = new ArrayList<>();
+
+                jsonList.add(lat);
+                jsonList.add(lng);
+
+                return jsonList;
+
+            } else if (parseIndex == 2){
+
+                JSONObject obj = object.getJSONObject("results");
+                String sunrise = obj.getString("sunrise");
+                String sunset = obj.getString("sunset");
+                List<String> jsonList = new ArrayList<>();
+
+
+                jsonList.add(sunrise);
+                jsonList.add(sunset);
+
+                return jsonList;
+
+            } else if (parseIndex == 3){
+                String obj = object.getString("rawOffset");
+                List<String> jsonList = new ArrayList<>();
+
+                jsonList.add(obj);
+
+                return jsonList;
+            } else {
+                return null;
+            }
+
+
+        } catch (JSONException e){
+            e.printStackTrace();
+
+            return null;
+        }
+
+    }
+}
